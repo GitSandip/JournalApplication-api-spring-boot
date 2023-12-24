110c923320a1b86750478c9f6b741d485679741b
sandip zalte <sandipzalte2002@gmail.com>
2023-12-24 23:46:07 +0530

    REST API created for CRUD operation

---
+++ src/main/java/aurora/JournalApp/controller/JournalEntryController.java
@@ -0,0 +1,65 @@
+package aurora.JournalApp.controller;
+
+import aurora.JournalApp.entity.JournalEntry;
+import aurora.JournalApp.repository.JournalEntryRepository;
+import aurora.JournalApp.service.JournalEntryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+
+    @Autowired
+    private JournalEntryService entry;
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
+    @GetMapping  ("/get-all-entry")
+    public List<JournalEntry> getAll(){
+        System.out.println(entry.getAllEntry());
+        return entry.getAllEntry();
+    }
+
+    @PostMapping ("add-entry")
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+
+        myEntry.setDate(LocalDateTime.now());
+        entry.saveEntry(myEntry);
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public ResponseEntity<JournalEntry> getById(@PathVariable String myId){
+        System.out.println("id get...");
+        return entry.getEntryById(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public ResponseEntity<JournalEntry> deleteById(@PathVariable String myId){
+
+        System.out.println("Record deleted id: "+myId);
+        return entry.deleteById(myId);
+    }
+
+    @PutMapping("update/id/{myId}")
+    public ResponseEntity<JournalEntry> update(@PathVariable String myId, @RequestBody JournalEntry newObj) {
+        Optional<JournalEntry> optionalOld = journalEntryRepository.findById(myId);
+
+        if (optionalOld.isPresent()) {
+            JournalEntry old = optionalOld.get();
+            old.setTitle(newObj.getTitle() != null && !newObj.getTitle().equals("") ? newObj.getTitle() : old.getTitle());
+            old.setContent(newObj.getContent() != null && !newObj.getContent().equals("") ? newObj.getContent() : old.getContent());
+            journalEntryRepository.save(old);
+            return ResponseEntity.ok(old);
+        } else {
+            System.out.println("There is No any Entry with id: " + myId);
+            return ResponseEntity.notFound().build(); // or handle it in another way, e.g., return a specific error response
+        }
+    }
+
+
+}
---
+++ src/main/java/aurora/JournalApp/service/JournalEntryService.java
@@ -0,0 +1,50 @@
+package aurora.JournalApp.service;
+
+import aurora.JournalApp.entity.JournalEntry;
+import aurora.JournalApp.repository.JournalEntryRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.http.ResponseEntity;
+import java.util.Optional;
+import java.util.List;
+
+@Component
+public class JournalEntryService {
+
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
+
+    public void saveEntry(JournalEntry j){
+        journalEntryRepository.save(j);
+    }
+
+    public List<JournalEntry> getAllEntry(){
+        return journalEntryRepository.findAll();
+    }
+
+    public ResponseEntity<JournalEntry> getEntryById(String ID) {
+        Optional<JournalEntry> optionalEntry = journalEntryRepository.findById(ID);
+
+        if (optionalEntry.isPresent()) {
+            JournalEntry entry = optionalEntry.get();
+            return ResponseEntity.ok(entry);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    public ResponseEntity<JournalEntry> deleteById(String deleteId){
+        Optional<JournalEntry> optionalEntry = journalEntryRepository.findById(deleteId);
+
+        if (optionalEntry.isPresent()) {
+            JournalEntry entry = optionalEntry.get();
+            journalEntryRepository.deleteById(deleteId);
+            return ResponseEntity.ok(entry);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
+}
