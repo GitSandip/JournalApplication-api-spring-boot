da63ff87add81b435fa5d27407554c5292940419
sandip zalte <sandipzalte2002@gmail.com>
2023-12-26 10:19:37 +0530

    project lombok added for implicit getter/setter

--- src/main/java/aurora/JournalApp/entity/JournalEntry.java
+++ src/main/java/aurora/JournalApp/entity/JournalEntry.java
@@ -1,50 +1,21 @@
 package aurora.JournalApp.entity;
-
-
+import lombok.Getter;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.Date;
 
-
+@Document   //(collection = "journal_db")
+@Getter
+@Setter
 public class JournalEntry {
 
-
-    private String id;
+    @Id
+    private ObjectId id;
     private String title;
     private String content;
     private LocalDateTime date;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
